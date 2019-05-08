053c75b377e07a78714b0e20cfff2cee60e506ee
irlartboy <ollie_king99@outlook.com>
2019-05-08 12:44:44 +1000

    First commit

---
+++ Assets/Scripts/LookAtDirection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// rotates any object at the same angle of target 
+public class LookAtDirection : MonoBehaviour
+{
+    public Transform target; 
+    private void LateUpdate()
+    {
+        transform.rotation = Quaternion.LookRotation(target.forward);
+    }
+}
