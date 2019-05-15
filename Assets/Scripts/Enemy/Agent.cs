ddaf8044bcdae7de70cf6502dfa3d43822fd9f53
irlartboy <ollie_king99@outlook.com>
2019-05-15 12:23:53 +1000

    update

---
+++ Assets/Scripts/Enemy/Agent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Agent : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+   
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+   public void GoHere(Vector3 position)
+    {
+        agent.SetDestination(position);
+    }
+}
---
+++ Assets/Scripts/Enemy/Director.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Director : MonoBehaviour
+{
+
+    public Agent agent;
+  
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // perform a raycast
+            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(camRay, out hit))
+            {
+                // get agent to go to hit point
+                agent.GoHere(hit.point);
+            }
+        }
+    }
+}
